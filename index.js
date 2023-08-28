1a3c6865903182d2e413408035bca7d969344e76
abik00 <abikp00@gmail.com>
2023-08-28 14:37:01 +0530

    javascript

---
+++ index.js
@@ -0,0 +1,6 @@
+var numberOfDrumButton=document.querySelectorAll(".drum").length;
+for(var i=0;i<numberOfDrumButton;i++){
+    document.querySelectorAll(".drum")[i].addEventListener("click",function () {
+        alert("hello"+i)
+    })
+}
